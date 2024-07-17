0c3d0f1d6b94166f85a286cb702b0ab3250e2e73
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-16 22:03:35 -0500

    First commit

---
+++ src/main/java/com/jfbarahonag/springboot/di/invoice/springboot_di_invoice/SpringbootDiInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.jfbarahonag.springboot.di.invoice.springboot_di_invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDiInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDiInvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfbarahonag/springboot/di/invoice/springboot_di_invoice/SpringbootDiInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfbarahonag.springboot.di.invoice.springboot_di_invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
